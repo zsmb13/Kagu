d47047485159308c1403cfe265b895a53ae6621d
zsmb13 <braunmarci@gmail.com>
2017-10-05 21:06:01 +0200

    Added dummy tests

---
+++ weblibProject/src/test/kotlin/BadTest.kt
@@ -0,0 +1,11 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class BadTest {
+
+    @Test
+    fun someBadTest() {
+        assertEquals(4, 2 + 2)
+    }
+
+}
---
+++ weblibProject/src/test/kotlin/GoodTest.kt
@@ -0,0 +1,11 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class GoodTest {
+
+    @Test
+    fun someGoodTest() {
+        assertEquals(4, 2 + 2)
+    }
+
+}
