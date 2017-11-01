5f3787b8221361943269e4ff50658b4cb6e89803
zsmb13 <braunmarci@gmail.com>
2017-11-01 10:39:49 +0100

    Improved caching of TemplateLoader to prevent simultaneous calls for the same template

--- weblibProject/src/main/kotlin/co/zsmb/weblib/services/templates/TemplateLoaderImpl.kt
+++ weblibProject/src/main/kotlin/co/zsmb/weblib/services/templates/TemplateLoaderImpl.kt
@@ -1,22 +1,35 @@
 package co.zsmb.weblib.services.templates
 
+import co.zsmb.weblib.core.InternalLogger
 import co.zsmb.weblib.core.jquery.JQ.parseHTML
 import co.zsmb.weblib.core.jquery.JQueryAjaxSettings
 import co.zsmb.weblib.core.jquery.jQuery
 import org.w3c.dom.Element
+
+private typealias Callback = (Element) -> Unit
 
 internal object TemplateLoaderImpl : TemplateLoader {
 
     private fun createEmptyObject() = js("return {}")
 
     private val cache = mutableMapOf<String, String>()
+    private val onGoingCalls = mutableMapOf<String, MutableList<Callback>>()
 
     override fun get(url: String, callback: (Element) -> Unit) {
 
         if (cache.containsKey(url)) {
+            InternalLogger.d(this, "Cache had key $url")
             returnResult(cache[url]!!, callback)
             return
         }
+
+        if (onGoingCalls.containsKey(url)) {
+            InternalLogger.d(this, "There were in flight calls for $url")
+            onGoingCalls[url]!! += callback
+            return
+        }
+
+        onGoingCalls[url] = mutableListOf(callback)
 
         val settings: JQueryAjaxSettings = createEmptyObject()
         val s = settings.apply {
@@ -26,7 +39,12 @@ internal object TemplateLoaderImpl : TemplateLoader {
 
         jQuery.ajax(s).done { html ->
             cache[url] = html
-            returnResult(html, callback)
+            InternalLogger.d(this, "Added to cache key $url")
+
+            onGoingCalls[url]!!.forEach { callback ->
+                returnResult(html, callback)
+            }
+            onGoingCalls.remove(url)
         }
 
     }
