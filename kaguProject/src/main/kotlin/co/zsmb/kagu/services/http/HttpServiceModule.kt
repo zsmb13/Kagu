1b0d84d1e459b2da93b942274fc43fff72ba724b
zsmb13 <braunmarci@gmail.com>
2017-11-18 16:23:14 +0100

    Fixed bad import

--- kaguProject/src/main/kotlin/co/zsmb/kagu/services/http/HttpServiceModule.kt
+++ kaguProject/src/main/kotlin/co/zsmb/kagu/services/http/HttpServiceModule.kt
@@ -1,12 +1,11 @@
 package co.zsmb.kagu.services.http
 
-import co.zsmb.kagu.services.logging.LoggerModule
 import co.zsmb.koinjs.dsl.module.Module
 
 internal object HttpServiceModule : Module() {
 
     override fun context() =
-            LoggerModule.declareContext {
+            declareContext {
                 provide { createHttpService() }
             }
 
